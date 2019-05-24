6438037051da376fbb37c49c2a8bfa23cd47a6c1
Avin-Techv <avin.techversant@gmail.com>
2019-05-24 11:22:16 +0530

    minor tweaks on required fields

--- python_test/python_test/forms.py
+++ python_test/python_test/forms.py
@@ -3,22 +3,13 @@ from .models import Client
 
 
 class ClientAddForm(forms.ModelForm):
+    street_name = forms.CharField(required=False)
+    suburb = forms.CharField(required=False)
+    postcode = forms.CharField(required=False)
+    state = forms.CharField(required=False)
+    client_contact_name = forms.CharField(required=False)
 
     class Meta:
         model = Client
         fields = ('client_name', 'street_name', 'suburb', 'postcode', 'state', 'client_contact_name', 'email',
                   'phone_number',)
-
-    def clean(self):
-        super(ClientAddForm, self).clean()
-
-        client_name = self.cleaned_data.get('client_name')
-        street_name = self.cleaned_data.get('street_name')
-        suburb = self.cleaned_data.get('suburb')
-        postcode = self.cleaned_data.get('postcode')
-        state = self.cleaned_data.get('state')
-        client_contact_name = self.cleaned_data.get('client_contact_name')
-        email = self.cleaned_data.get('email')
-        phone_number = self.cleaned_data.get('phone_number')
-
-        return self.cleaned_data
